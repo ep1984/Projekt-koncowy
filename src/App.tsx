5aa5855f5a997837d3a0abef2fd91069c8ac3046
Ewelina Pyrzak <pyrzake@gmail.com>
2024-08-04 18:09:41 +0200

    code refacor

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,39 @@
-import './App.css'
-import { Table } from './components/Table'
-
-export interface Employee {
-  id: string;
-  firstname: string;
-  lastname: string;
-  salary: number;
-  status: string;
-}
+import "./App.css";
+import { Table } from "./components/Table";
+import { Employee } from "./models/Employee";
 
 function App() {
-  const mockData: Employee [] = [
-  {
-id: '1',
-firstname: 'John',
-lastname: 'Doe',
-salary: 6000,
-status: 'Na urlopie'
-  },
-  {
-  id: '2',
-  firstname: 'Jan',
-  lastname: 'Kowalski',
-  salary: 16000,
-  status: 'Na urlopie'
-  },
-  {
-  id: '3',
-  firstname: 'Adam',
-  lastname: 'Nowak',
-  salary: 22000,
-  status: 'Na urlopie'
-  },
-  ]
+	const mockData: Employee[] = [
+		{
+			id: "1",
+			firstname: "Artur",
+			lastname: "Nowak",
+			salary: 6000,
+			status: "Na urlopie",
+		},
+		{
+			id: "2",
+			firstname: "Jan",
+			lastname: "Kowalski",
+			salary: 16000,
+			status: "Na urlopie",
+		},
+		{
+			id: "3",
+			firstname: "Tomasz",
+			lastname: "Jankowski",
+			salary: 22000,
+			status: "Na urlopie",
+		},
+	];
 
-  return (
-    <>
-      <h1>Employees</h1>
+	return (
+		<>
+			<h1>Employees</h1>
 
-      <Table data={mockData}></Table>
-    </>
-  )
+			<Table data={mockData}></Table>
+		</>
+	);
 }
 
-export default App
+export default App;
--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,31 +1,32 @@
-import { Employee } from '../App';
+import { Employee } from "../models/Employee";
 
-export interface TableProps {
-    data: Employee[];
+interface TableProps {
+	data: Employee[];
 }
 
-export function Table({data}: TableProps) {
-    return (
-    <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Firstname</th>
-            <th>Lastname</th>
-            <th>Salary</th>
-            <th>Status</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map(item => 
-          <tr>
-            <td>{item.id}</td>
-            <td>{item.firstname}</td>
-            <td>{item.lastname}</td>
-            <td>{item.salary}</td>
-            <td>{item.status}</td>
-          </tr>)}
-        </tbody>
-      </table>)
-
+export function Table({ data }: TableProps) {
+	return (
+		<table>
+			<thead>
+				<tr>
+					<th>ID</th>
+					<th>Firstname</th>
+					<th>Lastname</th>
+					<th>Salary</th>
+					<th>Status</th>
+				</tr>
+			</thead>
+			<tbody>
+				{data.map(item => (
+					<tr>
+						<td>{item.id}</td>
+						<td>{item.firstname}</td>
+						<td>{item.lastname}</td>
+						<td>{item.salary}</td>
+						<td>{item.status}</td>
+					</tr>
+				))}
+			</tbody>
+		</table>
+	);
 }
